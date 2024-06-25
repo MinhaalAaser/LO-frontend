9a6eca0b9efa25c41b6356fbbbc57f61899b749d
Minhaal Aaser <minhaala@gmail.com>
2024-06-24 22:00:49 -0500

    Another refactor

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -24,7 +24,7 @@ const LoginPage = () => {
     event.preventDefault();
     axios({
       method: 'POST',
-      url: 'https://salazarthorn.tplinkdns.com:5000/login',
+      url: 'salazarthorn.tplinkdns.com:5000/login',
       data: {
         email,
         pwd,
--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -26,7 +26,7 @@ function RegisterPage() {
     event.preventDefault();
     axios({
       method: 'post',
-      url: 'https://salazarthorn.tplinkdns.com:5000/register',
+      url: 'salazarthorn.tplinkdns.com:5000/register',
       data: {
         firstname,
         lastname,
